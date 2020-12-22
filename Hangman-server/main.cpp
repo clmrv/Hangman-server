40ab61c34090bfade898b706f0d82014e64b2ecb
clmrv <everay@wp.pl>
2020-12-22 19:38:10 +0100

    Initial Commit

---
+++ Hangman-server/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Hangman-server
+//
+//  Created by celmer on 22/12/2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
