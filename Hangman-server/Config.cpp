d2f2ed7eb0af2f9ffb1649f2df1c17a42febbc4b
clmrv <everay@wp.pl>
2021-01-23 15:47:37 +0100

    Add exception catching to config

--- Hangman-server/Config.cpp
+++ Hangman-server/Config.cpp
@@ -11,12 +11,15 @@ Config::Config(const char* filename) {
 
     PLOGD << "Reading config from file '" << filename << "'";
 
-    std::ifstream file(filename);
-
-    if(!file.good()) {
-        PLOGE << "Error opening file " << filename << ", using default settings";
-        return;
-    }
+    std::ifstream file;
+    
+    try {
+        file.open(filename);
+        if(!file.good()) {
+            PLOGE << "Error opening file " << filename << ", using default settings";
+            return;
+        }
+    } catch (const std::exception& e) { PLOGW << "Could not load config file"; }
 
     std::string buf;
     while(getline(file, buf)) {
@@ -26,8 +29,10 @@ Config::Config(const char* filename) {
         auto value = std::string(line.substr(delimeter + 1));
 
         if(key == "port") {
-            port = std::stoi(value);
-            PLOGV << "\tport: " << port;
+            try {
+                port = std::stoi(value);
+                PLOGV << "\tport: " << port;
+            } catch (const std::exception& e) { PLOGW << "Could not load port from config"; }
         }
         else if(key == "langs") {
 
@@ -44,28 +49,36 @@ Config::Config(const char* filename) {
 
         }
         else if(key == "wordLength") {
-            auto bar = value.find_first_of('-');
-            roomSettings.wordLength[0] = std::stoi(value.substr(0, bar));
-            roomSettings.wordLength[1] = std::stoi(value.substr(bar + 1));
-            PLOGV << "\tword length: " << (int)roomSettings.wordLength[0] << " - " << (int)roomSettings.wordLength[1];
+            try {
+                auto bar = value.find_first_of('-');
+                roomSettings.wordLength[0] = std::stoi(value.substr(0, bar));
+                roomSettings.wordLength[1] = std::stoi(value.substr(bar + 1));
+                PLOGV << "\tword length: " << (int)roomSettings.wordLength[0] << " - " << (int)roomSettings.wordLength[1];
+            } catch (const std::exception& e) { PLOGW << "Could not load wordLength from config"; }
         }
         else if(key == "gameTime") {
-            auto bar = value.find_first_of('-');
-            roomSettings.gameTime[0] = std::stoi(value.substr(0, bar));
-            roomSettings.gameTime[1] = std::stoi(value.substr(bar + 1));
-            PLOGV << "\tgame time: " << (int)roomSettings.gameTime[0] << " - " << (int)roomSettings.gameTime[1] << " seconds";
+            try {
+                auto bar = value.find_first_of('-');
+                roomSettings.gameTime[0] = std::stoi(value.substr(0, bar));
+                roomSettings.gameTime[1] = std::stoi(value.substr(bar + 1));
+                PLOGV << "\tgame time: " << (int)roomSettings.gameTime[0] << " - " << (int)roomSettings.gameTime[1] << " seconds";
+            } catch (const std::exception& e) { PLOGW << "Could not load gameTime from config"; }
         }
         else if(key == "healthPoints") {
-            auto bar = value.find_first_of('-');
-            roomSettings.healthPoints[0] = std::stoi(value.substr(0, bar));
-            roomSettings.healthPoints[1] = std::stoi(value.substr(bar + 1));
-            PLOGV << "\thealth points: " << (int)roomSettings.healthPoints[0] << " - " << (int)roomSettings.healthPoints[1];
+            try {
+                auto bar = value.find_first_of('-');
+                roomSettings.healthPoints[0] = std::stoi(value.substr(0, bar));
+                roomSettings.healthPoints[1] = std::stoi(value.substr(bar + 1));
+                PLOGV << "\thealth points: " << (int)roomSettings.healthPoints[0] << " - " << (int)roomSettings.healthPoints[1];
+            } catch (const std::exception& e) { PLOGW << "Could not load healthPoints from config"; }
         }
         else if(key == "players") {
-            auto bar = value.find_first_of('-');
-            roomSettings.playerCount[0] = std::stoi(value.substr(0, bar));
-            roomSettings.playerCount[1] = std::stoi(value.substr(bar + 1));
-            PLOGV << "\tplayers: " << (int)roomSettings.playerCount[0] << " - " << (int)roomSettings.playerCount[1];
+            try {
+                auto bar = value.find_first_of('-');
+                roomSettings.playerCount[0] = std::stoi(value.substr(0, bar));
+                roomSettings.playerCount[1] = std::stoi(value.substr(bar + 1));
+                PLOGV << "\tplayers: " << (int)roomSettings.playerCount[0] << " - " << (int)roomSettings.playerCount[1];
+            } catch (const std::exception& e) { PLOGW << "Could not load players from config"; }
         }
         else {
             PLOGV << "Undefined key udring loading config: '" << key << "'";
